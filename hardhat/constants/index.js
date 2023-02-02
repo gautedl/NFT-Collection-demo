201120e6185653ad00033ba5ebcd0c62075449f7
Gaute Lorentsen <gudelo97@gmail.com>
2023-02-02 09:25:33 +0100

    created nft contract

---
+++ hardhat/constants/index.js
@@ -0,0 +1,7 @@
+const WHITELIST_CONTRACT_ADDRESS = '0x871963B652505e10b7d11B7b7FAF2b4Df0523B0d';
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = 'https://nft-collection-sneh1999.vercel.app/api/';
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
+
+contract = '0x272D9E9DFF6165E62e690d2895214f2c6AB6a998';
