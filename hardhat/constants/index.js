5a58c69da0bd8b3cef557d19506d925f5c4421c5
Gaute Lorentsen <gudelo97@gmail.com>
2023-02-03 08:30:34 +0100

    Added frontend and connected with next.js. Working dapp

--- hardhat/constants/index.js
+++ hardhat/constants/index.js
@@ -3,5 +3,3 @@ const WHITELIST_CONTRACT_ADDRESS = '0x871963B652505e10b7d11B7b7FAF2b4Df0523B0d';
 const METADATA_URL = 'https://nft-collection-sneh1999.vercel.app/api/';
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
-
-contract = '0x272D9E9DFF6165E62e690d2895214f2c6AB6a998';
